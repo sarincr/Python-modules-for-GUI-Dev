2bc56126d1252d580500a09f5323fa0d7a2abe56
sarincr <sarinrays@gmail.com>
2021-08-17 15:46:14 +0530

    Pyglet

---
+++ Pyglet/main.py
@@ -0,0 +1,18 @@
+import pyglet
+  
+new_window = pyglet.window.Window()
+  
+label = pyglet.text.Label('Hello, World !',
+                          font_name ='Cooper',
+                          font_size = 16,
+                          x = new_window.width//2, 
+                          y = new_window.height//2,
+                          anchor_x ='center', 
+                          anchor_y ='center')
+  
+@new_window.event
+def on_draw():
+    new_window.clear()
+    label.draw()
+  
+pyglet.app.run()
